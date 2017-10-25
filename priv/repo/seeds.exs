f114009c000d92847f7e915a1d4144648c727f12
Vladislav St <v.stafg@gmail.com>
2017-10-25 17:24:44 +0300

    Seeds for dev DB testing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,37 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will halt execution if something goes wrong.
+
+
+Takso.Repo.insert!(%Takso.Trip{
+    date: ~D[2000-01-01],
+    pickup_address: "Kalevi",
+    dropoff_address: "Mihkli",
+    pickup_time: ~T[23:00:07.001],
+    dropoff_time: ~T[23:00:07.001],
+    est_pickup_time: ~T[23:00:07.001],
+    est_duration: ~T[00:00:07.001],
+    taxi_id: 1
+})
+
+Takso.Repo.insert!(%Takso.Trip{
+    date: ~D[2000-08-01],
+    pickup_address: "Kalevis",
+    dropoff_address: "Mihklis",
+    pickup_time: ~T[23:00:07.001],
+    dropoff_time: ~T[23:00:07.001],
+    est_pickup_time: ~T[23:00:07.001],
+    est_duration: ~T[00:00:07.001],
+    taxi_id: 1
+})
+
+Takso.Repo.insert!(%Takso.Trip{
+    date: ~D[2000-08-01],
+    pickup_address: "Naituse",
+    dropoff_address: "Klebo",
+    pickup_time: ~T[23:00:07.001],
+    dropoff_time: ~T[23:00:07.001],
+    est_pickup_time: ~T[23:00:07.001],
+    est_duration: ~T[00:00:07.001],
+    taxi_id: 1
+})
