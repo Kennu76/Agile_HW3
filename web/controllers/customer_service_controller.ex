5d6a36d6c68732bd254f920716903cf0146c7f37
Vladislav St <v.stafg@gmail.com>
2017-10-25 17:23:31 +0300

    Tested only with localhost dev DB

--- web/controllers/customer_service_controller.ex
+++ web/controllers/customer_service_controller.ex
@@ -1,22 +1,31 @@
 defmodule Takso.CustomerServiceController do
   use Takso.Web, :controller
-  import Ecto.Query, only: [from: 2]
-  alias Takso.{CSRequest, Trip}
+  use Ecto.Schema
+  import Ecto.Query
+
+  alias Takso.CSRequest
 
   def lost_and_found(conn, params) do
-    if (params != %{}) do
+    changeset = CSRequest.changeset(%CSRequest{})
+    if ( params != %{} ) do
       %{"cs_request" => request} = params
       %{"date_of_trip" => date} = request
-      query = from t in Trip, where: t.date == ^date
-      trips = Repo.all(from t in query, select: t)
-      changeset = CSRequest.changeset(%CSRequest{})
-      render conn, "lost_and_found.html", changeset: changeset, trips: trips
-      IO.inspect query 
-    else 
-    
-      changeset = CSRequest.changeset(%CSRequest{})
-      trips = []
-      render conn, "lost_and_found.html", changeset: changeset, trips: trips
+      %{"pickup_address" => address } = request
+      if date == "" do
+        query = from t in Takso.Trip, where: t.pickup_address == ^address, select: t
+      else
+        date = date |> Timex.parse!("%Y-%m-%d", :strftime) |> Ecto.Date.cast!
+        #IO.inspect(date)
+        if address == "" do
+          query = from t in Takso.Trip, where: t.date == ^date, select: t
+        else
+          query = from t in Takso.Trip, where: t.date == ^date, where: t.pickup_address == ^address, select: t
+        end
+      end
+      trips = Repo.all(query)
+    else
+      trips = Repo.all(Takso.Trip)
     end
+    render conn, "lost_and_found.html", changeset: changeset, trips: trips
   end
 end
