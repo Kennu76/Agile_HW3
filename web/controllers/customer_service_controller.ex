b02160f9b92b3f9ba97017d0de99c4b86a891b06
Vladislav Stafinjak <v.stafg@gmail.com>
2017-10-30 21:55:15 +0200

    Fixed bug when after a query a empty params are sent and no trips are shown

--- web/controllers/customer_service_controller.ex
+++ web/controllers/customer_service_controller.ex
@@ -13,20 +13,21 @@ defmodule Takso.CustomerServiceController do
       %{"pickup_address" => address } = request
       if date == "" do
         query = from t in Takso.Trip, where: t.pickup_address == ^address, select: t
-      else  
+        if address == "" do
+          query = Takso.Trip
+        else
+        end
+      else
         date = date |> Timex.parse!("%Y-%m-%d", :strftime) |> Ecto.Date.cast!
         #IO.inspect(date)
         if address == "" do
-          
           query = from t in Takso.Trip, where: t.date == ^date, select: t
           #IO.inspect query
         else
-          
           query = from t in Takso.Trip, where: t.date == ^date and t.pickup_address == ^address, select: t
           #IO.inspect query
         end
       end
-      
       #IO.inspect query
       trips = Repo.all(query)
       changeset = CSRequest.changeset(%CSRequest{},request)
@@ -34,14 +35,12 @@ defmodule Takso.CustomerServiceController do
       #IO.inspect Takso.Trip
       trips = Repo.all(Takso.Trip)
     end
-    
     render conn, "lost_and_found.html", changeset: changeset, trips: trips
   end
 
 
   def scam_analysis(conn, params) do
     changeset = CSRequest.changeset(%CSRequest{})
-    
     render conn, "scam_analysis.html", changeset: changeset
   end
 end
